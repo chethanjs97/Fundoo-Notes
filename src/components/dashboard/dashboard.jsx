77d805b543424a6a6304a572227dee95f0f5090a
chethanjs97 <chethanjschethu@gmail.com>
2021-01-06 13:40:08 +0530

    created the registration, signin, forgot password pages with validation

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -85,7 +85,7 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(0, 1),
     // necessary for content to be below app bar
     ...theme.mixins.toolbar,
-    
+     
   },
   content: {
     flexGrow: 1,
